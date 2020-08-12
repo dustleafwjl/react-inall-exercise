9fae426e1a1e465d7fadf5b1070bccbec0a989b6
dustleafwjl <wangjianglin_priv@qq.com>
2020-08-12 21:24:12 +0800

    [jianglin wang] feat: finished all

--- src/app/Calculator/Calculator.js
+++ src/app/Calculator/Calculator.js
@@ -2,44 +2,59 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import './calculator.module.less';
 
-const Calculator = () => {
-  return (
-  <div className="calculator">
-    <header>在线计算器</header>
-    <main>
-      <div>
-        <input></input>
-      </div>
-      <div>
-        <div className="key yellow">+</div>
-        <div className="key yellow">-</div>
-        <div className="key yellow">x</div>
-      </div>
-      <div>
-        <div className="key">7</div>
-        <div className="key">8</div>
-        <div className="key">9</div>
-      </div>
-      <div>
-        <div className="key">4</div>
-        <div className="key">5</div>
-        <div className="key">6</div>
-      </div>
-      <div>
-        <div className="key">1</div>
-        <div className="key">2</div>
-        <div className="key">3</div>
-      </div>
-      <div>
-        <div className="key">0</div>
-        <div className="key clear">clear</div>
-        <div className="key equal">=</div>
-      </div>
-    </main>
-    <footer>
-      <Link to="/">回到主页</Link>
-    </footer>
-  </div>);
+class Calculator extends React.Component  {
+  state = {
+    number: ''
+  }
+  addStrToNum = (str) => {
+    this.setState({number: this.state.number + str});
+  }
+  clearStr = () => {
+    this.setState({number: ''});
+  }
+  getResult = () => {
+    let str = this.state.number;
+    this.setState({number: eval(str.replace(/x/, '*'))});
+  }
+  render() {
+    return (
+      <div className="calculator">
+        <header>在线计算器</header>
+        <main>
+          <div>
+            <div className="input">{this.state.number}</div>
+          </div>
+          <div>
+            <div className="key yellow" onClick={() => this.addStrToNum('+')}>+</div>
+            <div className="key yellow" onClick={() => this.addStrToNum('-')}>-</div>
+            <div className="key yellow" onClick={() => this.addStrToNum('x')}>x</div>
+          </div>
+          <div>
+            <div className="key" onClick={() => this.addStrToNum('7')}>7</div>
+            <div className="key" onClick={() => this.addStrToNum('8')}>8</div>
+            <div className="key" onClick={() => this.addStrToNum('8')}>9</div>
+          </div>
+          <div>
+            <div className="key" onClick={() => this.addStrToNum('4')}>4</div>
+            <div className="key" onClick={() => this.addStrToNum('5')}>5</div>
+            <div className="key" onClick={() => this.addStrToNum('6')}>6</div>
+          </div>
+          <div>
+            <div className="key" onClick={() => this.addStrToNum('1')}>1</div>
+            <div className="key" onClick={() => this.addStrToNum('2')}>2</div>
+            <div className="key" onClick={() => this.addStrToNum('3')}>3</div>
+          </div>
+          <div>
+            <div className="key" onClick={() => this.addStrToNum('0')}>0</div>
+            <div className="key clear" onClick={this.clearStr}>clear</div>
+            <div className="key equal" onClick={this.getResult}>=</div>
+          </div>
+        </main>
+        <footer>
+          <Link to="/">回到主页</Link>
+        </footer>
+      </div>);
+  }
 };
 
 export default Calculator;
--- src/app/Countdown/Countdown.js
+++ src/app/Countdown/Countdown.js
@@ -2,30 +2,79 @@ import React from 'react';
 import {Link} from "react-router-dom"
 import './countdown.module.less';
 
-const Countdown = () => {
-  return (
-  <div className="countdown">
-    <header>在线倒计时器</header>
-    <main>
-      <div>
-        <div className="set-time">
-          <label>设置时间</label>
-          <input></input>
-        </div>
-        <div className="ctr-btn">
-          <div>Start</div>
-          <div style={{display: "none"}}>End</div>
-        </div>
-      </div>
-      <div>
-        {42}
-        Seconds
-      </div>
-    </main>
-    <footer>
-      <Link to="/">回到主页</Link>
-    </footer>
-  </div>);
+class Countdown extends React.Component {
+
+  state = {
+    time: '?',
+    inputTime: '',
+    isEnd: false,
+    timer: null
+  }
+
+  handleTimeChange = (e) => {
+    this.setState({
+      inputTime: e.target.value,
+      isEnd: false
+    })
+  }
+  setTimer = () => {
+    let timer = setInterval(() => {
+      this.setState({
+        time: this.state.time - 1
+      });
+      if(this.state.time <= 0) {
+        this.end();
+      }
+    }, 1000);
+    this.setState({
+      timer
+    })
+  }
+  clearTimer = () => {
+    console.log(this.state.timer)
+    clearInterval(this.state.timer)
+  }
+  start = () => {
+    this.clearTimer()
+    if(!this.state.inputTime) return;
+    this.setState({
+      time: this.state.inputTime
+    })
+    this.setTimer()
+  }
+  end = () => {
+    this.setState({
+      isEnd: true,
+      inputTime: ''
+    })
+    this.clearTimer()
+  }
+  render() {
+    return (
+      <div className="countdown">
+        <header>在线倒计时器</header>
+        <main>
+          <div>
+            <div className="set-time">
+              <label>设置时间</label>
+              <input value={this.state.inputTime} onChange={this.handleTimeChange}></input>
+            </div>
+            <div className="ctr-btn">
+              {
+                this.state.isEnd ? <div onClick={this.end}>End</div> : <div onClick={this.start}>Start</div>
+              }
+            </div>
+          </div>
+          <div>
+            {this.state.time}
+            <span>Seconds</span>
+          </div>
+        </main>
+        <footer>
+          <Link to="/">回到主页</Link>
+        </footer>
+      </div>);
+  }
 };
 
 export default Countdown;
